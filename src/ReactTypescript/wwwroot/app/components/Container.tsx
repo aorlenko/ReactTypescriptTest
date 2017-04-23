75476dc506927e4c1cd94667e2e8d5d97eeb5952
aorlenko <orlenko.andrew@gmail.com>
2017-04-23 03:16:07 +0300

    Work

---
+++ src/ReactTypescript/wwwroot/app/components/Container.tsx
@@ -0,0 +1,34 @@
+﻿import * as React from 'react';
+
+export interface ContainerSettings extends React.Props<Container> {
+    fullScreen?: boolean;
+    refresh?: boolean;
+}
+
+export class Container extends React.Component<ContainerSettings, undefined>{
+    constructor(props: ContainerSettings) {
+        super(props);
+        console.log(props);
+    }
+
+    render() {
+        let padRight = {
+            paddingRight: '5px'
+        };
+        
+        return (
+            <div className="panel panel-primary">
+                <div className="panel-heading">&nbsp;
+                    <div className="pull-right">
+                        {this.props.refresh && <span style={padRight} className="glyphicon glyphicon-refresh" aria-hidden="true"></span>}
+                        {this.props.fullScreen && <span className="glyphicon glyphicon-resize-full" aria-hidden="true"></span>}
+                    </div>
+                </div>
+                <div className="panel-body">
+                    fullScreen: {String(this.props.fullScreen)}<br />
+                    refresh:{String(this.props.refresh)}
+                </div>
+            </div>
+        )
+    }
+}
--- src/ReactTypescript/wwwroot/app/components/Main.tsx
+++ src/ReactTypescript/wwwroot/app/components/Main.tsx
@@ -1,17 +1,50 @@
 ﻿import * as React from "react";
+import { Container, ContainerSettings } from './Container';
 
-export class Main extends React.Component<undefined, undefined>{
+export class Main extends React.Component<any, any>{
     constructor() {
         super();
+
+        this.state = {
+        fullScreen:false
+        }
+    }
+
+    componentDidMount() {
+        var xhr = new XMLHttpRequest();
+        xhr.open('get', 'http://localhost:3798/components/grid.json', true);
+        xhr.onload = function () {
+            var data = JSON.parse(xhr.responseText);
+            console.log(data);
+
+            this.setState({
+                fullScreen: data.generalFeatures.fullScreen,
+                refresh: data.generalFeatures.manualRefresh
+            });
+
+            console.log(this.state);
+
+        }.bind(this);
+        xhr.send();
     }
 
     render() {
+        
+
         return (
             <div className="row">
-                <div className="panel panel-default">
-                    <div className="panel-body">
-                        Basic panel example
-                    </div>
+                <div className="col-md-4">
+                    <Container
+                        fullScreen={this.state.fullScreen}
+                        refresh={this.state.refresh}>
+                        <div>hello ffrom container</div>
+                    </Container>
+                </div>
+                <div className="col-md-4">
+                    2
+                </div>
+                <div className="col-md-4">
+                    3
                 </div>
             </div>
         )
